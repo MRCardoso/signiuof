219b79c60da8b5a2391d41b2014c9dee8ac0e399
MRCardoso <cardozinhos.mrc@gmail.com>
2018-01-01 01:45:17 -0200

    five test

--- copyFiles.js
+++ copyFiles.js
@@ -1,6 +1,6 @@
-require('fs').writeFile('./test.txt', 'message', (err,d) => {  
+require('fs').writeFile('../../test.txt', 'message', (err,d) => {  
     if (err){
         console.log('LOG-ERR:', err);
     }
-    console.log('LOG-SUCCESS:', d);
+    console.log('LOG-SUCCESS');
 });
