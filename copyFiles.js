3cbe0aa39a051cdef3c713e14803dde313d18050
MRCardoso <cardozinhos.mrc@gmail.com>
2018-01-06 03:46:56 -0200

    adjust forder copy

--- copyFiles.js
+++ copyFiles.js
@@ -1,21 +1,24 @@
 let folder = "app/views";
 let fs = require("fs");
 let extra = require("fs-extra");
+let colors = require('colors');
 
-console.log(require("./lib/utils/helpers").getBasePath(), process.env.PWD);
-// if( require("./lib/utils/helpers").getBasePath() != process.env.PWD ){
-//     if( !fs.existsSync(`../../${folder}`) )
-//     {
-//         console.log("Init Copy");
-//         extra.copy(`./example/${folder}`, `../../${folder}`, function (err) {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log("Copy Success!");
-//             }
-//         });
-//     }
-//     else{
-//         console.log(`Already exists ../../${folder}`);
-//     }
-// }
+if( require("./lib/utils/helpers").getBasePath() != process.env.PWD ){
+    if( !fs.existsSync(`../../${folder}`) )
+    {
+        console.log("Init Copy".blue);
+        extra.copy(`./example/${folder}`, `../../${folder}`, function (err) {
+            if (err) {
+                console.log("erro in copy the files, try to copy manually the folder 'example/app'".red,err);
+            } else {
+                console.log("Copy Success!".green);
+            }
+        });
+    }
+    else{
+        console.log(`Already exists ../../${folder}`.blue);
+    }
+}
+else{
+    console.log("You are in the signiuof package".blue);
+}
