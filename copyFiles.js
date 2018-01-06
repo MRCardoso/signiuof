0eb8b61d715f29eb793c5c33b93f871b40df34a5
MRCardoso <cardozinhos.mrc@gmail.com>
2018-01-06 13:18:23 -0200

    Fix ajusts in the copy of the files

--- copyFiles.js
+++ copyFiles.js
@@ -1,23 +1,50 @@
 let folder = "app/views";
+let origin = `./example/${folder}/`;
+let destination = `../../${folder}/`;
 let fs = require("fs");
-let extra = require("fs-extra");
-let colors = require('colors');
+let shelljs = require("shelljs");
+require('colors');
 
-if( require("./lib/utils/helpers").getBasePath() != process.env.PWD ){
-    if( !fs.existsSync(`../../${folder}`) )
-    {
-        console.log("Init Copy".blue);
-        extra.copy(`./example/${folder}`, `../../${folder}`, function (err) {
-            if (err) {
-                console.log("erro in copy the files, try to copy manually the folder 'example/app'".red,err);
-            } else {
-                console.log("Copy Success!".green);
+if( require("./lib/utils/helpers").getBasePath() != process.env.PWD )
+{
+    let walkSync = function(dir, fileItens){
+        let files = fs.readdirSync(dir);
+        fileItens = fileItens || [];
+
+        files.forEach((file) => {
+            if(fs.statSync(dir+file).isDirectory()){
+                fileItens = walkSync(dir+file+"/", fileItens);
+            }
+            else{
+                fileItens.push({destiny: (dir.replace(origin,'')), file: file});
             }
         });
+        return fileItens;
     }
-    else{
-        console.log(`Already exists ../../${folder}`.blue);
-    }
+    let itens = walkSync(origin);
+
+    itens.forEach(i =>
+    {
+        let f = (i.destiny != "" ? destination+i.destiny : destination);
+        let d = `${f}${i.file}`;
+        if( !fs.existsSync(d))
+        {
+            if(!fs.existsSync(f)){
+                shelljs.mkdir("-p",f);
+            }
+        
+            fs.copyFile(`${origin}${f}${i.file}`, d, err =>{
+                if (err) {
+                    console.log("erro in copy the files, try to copy manually the folder 'example/app'".red,err);
+                } else {
+                    console.log("Copy Success!".green);
+                }
+            })
+        }
+        else{
+            console.log(`File ${d} already exists`.blue);
+        }
+    });
 }
 else{
     console.log("You are in the signiuof package".blue);
